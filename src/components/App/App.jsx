45e5b5b96ba81464f78b52e42e9d4e52d8aec699
MariyaVdovenko <m.vdovenko@ukr.net>
2019-10-26 12:10:41 +0300

    test fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 
 const App = () => (
   <div className="App">
-    <Profile user={user} />
+    <Profile props={user} />
   </div>
 );
 
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import './Profile.css';
+
+const Profile = ({ props }) => {
+  const avatar = props.avatar;
+  const name = props.name;
+  const tag = props.tag;
+  const location = props.location;
+  const followers = props.stats.followers;
+  const views = props.stats.views;
+  const likes = props.stats.likes;
+
+  return (
+    <div className="Profile">
+      <div className="description">
+        <img src={avatar} alt="user avatar" className="Profile__avatar" />
+        <p className="Profile__name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="Profile__location">{location}</p>
+        <ul className="Profile__stats">
+          <li className="Profile__stats--item">
+            <span className="Profile__stats--label">Followers </span>
+            <span className="Profile__stats--quantity">{followers}</span>
+          </li>
+          <li className="Profile__stats--item">
+            <span className="Profile__stats--label">Views </span>
+            <span className="Profile__stats--quantity">{views}</span>
+          </li>
+          <li className="Profile__stats--item">
+            <span className="Profile__stats--label">Likes </span>
+            <span className="Profile__stats--quantity">{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  name: 'anonymous',
+  location: 'unknown',
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
+
+export default Profile;
